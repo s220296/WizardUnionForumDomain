a7f3937e74799f298d4d6518e299504a6c109478
Tom <s220296@students.aie.edu.au>
2024-11-07 11:01:41 +1100

    Testing user input for messaging

--- Wizard Union Test Application/Application/MessagingTest.cs
+++ Wizard Union Test Application/Application/MessagingTest.cs
@@ -12,20 +12,16 @@ public static class MessagingTest
         UserProfile w1 = new UserProfile(wizards[0], new WizardMessager(wizards[0].Item));
         UserProfile w2 = new UserProfile(wizards[1], new WizardMessager(wizards[1].Item));
 
-        Console.WriteLine($"{w1.Wizard.Name} is sending a message to {w2.Wizard.Name}.");
-
-        TextMessage message = new TextMessage("Hello friend!");
-        if (!message.TrySend(w1.Messager, w2.Messager))
+        while (true)
         {
-            Console.WriteLine("Message not received");
-        }
+            Console.WriteLine("\nYou are Ryan and you are messaging Harry.");
+            Console.Write("Message: ");
+            TextMessage newMessage = new TextMessage(Console.ReadLine());
+            Console.WriteLine("");
 
-        Console.WriteLine($"\n{w2.Wizard.Name} is replying to {w1.Wizard.Name}");
+            Console.Clear();
 
-        TextMessage reply = new TextMessage("Hello to you!");
-        if (!reply.TrySend(w2.Messager, w1.Messager))
-        {
-            Console.WriteLine("Reply not received");
+            newMessage.TrySend(w1.Messager, w2.Messager);
         }
     }
 }
