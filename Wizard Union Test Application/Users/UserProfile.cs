5b249df119ed08c87305c8d86ecf7d9df1b5d8c3
Tom <s220296@students.aie.edu.au>
2024-11-08 10:31:42 +1100

    Clean up and added IDs to UserProfile
    - Enabling easier use with DB, which I have been setting up tables for

--- Wizard Union Test Application/Users/UserProfile.cs
+++ Wizard Union Test Application/Users/UserProfile.cs
@@ -5,14 +5,12 @@ namespace WU_Test;
 public sealed class UserProfile
 {
     public WizardMessager Messager { get; private set; }
-    public Wizard Wizard => m_wizardID.Item;
-
-    private IDItem<Wizard> m_wizardID;
+    public IDItem<Wizard> Wizard { get; private set; }
 
     private UserProfile() { throw new ArgumentNullException($"Please initialize item: {nameof(UserProfile)} {typeof(UserProfile)}."); }
 
     public UserProfile(IDItem<Wizard> _wizard, WizardMessager _messager)
     {
-        (m_wizardID, Messager) = (_wizard, _messager);
+        (Wizard, Messager) = (_wizard, _messager);
     }
 }
