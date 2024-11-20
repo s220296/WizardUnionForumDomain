2b4381f4b96547f7f3a0ac3e5d52d3204314b556
Tom <s220296@students.aie.edu.au>
2024-11-21 10:22:06 +1100

    Tested live updating with 2 consoles

--- Wizard Union Test Application/Application/FullMessagerDemo.cs
+++ Wizard Union Test Application/Application/FullMessagerDemo.cs
@@ -8,6 +8,13 @@ public static class FullMessagerDemo
     private static UserProfile s_user;
     private static UserProfile s_recipient;
     private static string s_preWrite;
+    private static string s_messagesPreWrite;
+    private static bool s_keepRefreshing;
+
+    private class CancelToken
+    {
+        public bool cancel = false;
+    }
 
     private static bool SelectWizard(string _name, bool _recipient)
     {
@@ -41,6 +48,8 @@ public static class FullMessagerDemo
         {
             RunLoop(_args);
 
+            s_keepRefreshing = false;
+
             Console.WriteLine("Write 'exit' to exit or press enter to continue");
             if (Console.ReadLine().Equals("exit")) return;
 
@@ -51,8 +60,10 @@ public static class FullMessagerDemo
     private static void RunLoop(int _args)
     {
         s_preWrite = new string("");
+        s_messagesPreWrite = new string("");
         s_user = null;
         s_recipient = null;
+        s_keepRefreshing = false;
 
         AddToPreWrite("Write 'quit' to quit\n");
 
@@ -80,8 +91,64 @@ public static class FullMessagerDemo
         AddToPreWrite($"Wizard {s_recipient.Wizard.Item.Name} is the recipient\n\n");
         Console.WriteLine($"Wizard {s_recipient.Wizard.Item.Name} was successfully selected as the recipient");
 
+        AddDbMessagesToPreWrite();
+
         Console.Clear();
-        WritePreWrite();
+
+        while (true)
+        {
+            string input = new string("");
+
+            s_keepRefreshing = true;
+
+            do
+            {
+                AddDbMessagesToPreWrite();
+
+                WritePreWrite();
+
+                Console.WriteLine($"Send a message to {s_recipient.Wizard.Item.Name} as {s_user.Wizard.Item.Name}.");
+
+                // Will continue to refresh every second
+                CancelToken token = new CancelToken();
+                RefreshPage(1000, token);
+
+                input = Console.ReadLine();
+
+                token.cancel = true;
+
+                if (input.Equals("quit")) return;
+            }
+            while (string.IsNullOrWhiteSpace(input));
+
+            DataSubmission.RecordTextMessage(new TextMessage(input), s_user.Messager, s_recipient.Messager);
+
+            Console.Clear();
+        }
+    }
+
+    private static async void RefreshPage(int _milliInterval, CancelToken _token)
+    {
+        while (s_keepRefreshing && !_token.cancel)
+        {
+            await Task.Delay(_milliInterval);
+
+            string prePreWrite = GetFullPreWrite();
+
+            await Task.Run(AddDbMessagesToPreWrite);
+
+            if (!prePreWrite.Equals(GetFullPreWrite()))
+            {
+                Console.Clear();
+                WritePreWrite();
+                Console.WriteLine($"Send a message to {s_recipient.Wizard.Item.Name} as {s_user.Wizard.Item.Name}.");
+            }
+        }
+    }
+
+    private static void AddDbMessagesToPreWrite()
+    {
+        s_messagesPreWrite = new string("");
 
         IDItem<TextMessage>[] messagesToRecipient = DataAcquirer.GetMessagesToWizardFrom(s_user.Wizard.ID, s_recipient.Wizard.ID);
         IDItem<TextMessage>[] messagesFromRecipient = DataAcquirer.GetMessagesToWizardFrom(s_recipient.Wizard.ID, s_user.Wizard.ID);
@@ -92,7 +159,7 @@ public static class FullMessagerDemo
 
         int toRecip = 0, fromRecip = 0;
 
-        for(int i = 0; i < messages.Length; i++)
+        for (int i = 0; i < messages.Length; i++)
         {
             int toID = toRecip < messagesToRecipient.Length ? messagesToRecipient[toRecip].ID : int.MaxValue;
             int fromID = fromRecip < messagesFromRecipient.Length ? messagesFromRecipient[fromRecip].ID : int.MaxValue;
@@ -113,40 +180,17 @@ public static class FullMessagerDemo
             // There should be no case where toID and fromID are equal, unless messaging self (prevented in SelectWizard())
 
             if (messages[i].toRecip)
-                AddToPreWrite($"{s_user.Wizard.Item.Name} to {s_recipient.Wizard.Item.Name}:\n");
+                AddToMessagesPreWrite($"{s_user.Wizard.Item.Name} to {s_recipient.Wizard.Item.Name}:\n");
             else // If recipient to sender
-                AddToPreWrite($"{s_recipient.Wizard.Item.Name} to {s_user.Wizard.Item.Name}:\n");
-            AddToPreWrite($"{messages[i].text.Text}\n\n");
-        }
-
-        Console.Clear();
-
-        while (true)
-        {
-            WritePreWrite();
-
-            Console.WriteLine($"Send a message to {s_recipient.Wizard.Item.Name} as {s_user.Wizard.Item.Name}.");
-
-            string input = "";
-
-            do
-            {
-                input = Console.ReadLine();
-
-                if (input.Equals("quit")) return;
-            }
-            while (string.IsNullOrWhiteSpace(input));
-
-            DataSubmission.RecordTextMessage(new TextMessage(input), s_user.Messager, s_recipient.Messager);
-
-            AddToPreWrite($"{s_user.Wizard.Item.Name} to {s_recipient.Wizard.Item.Name}:\n");
-            AddToPreWrite($"{input}\n\n");
-
-            Console.Clear();
+                AddToMessagesPreWrite($"{s_recipient.Wizard.Item.Name} to {s_user.Wizard.Item.Name}:\n");
+            AddToMessagesPreWrite($"{messages[i].text.Text}\n\n");
         }
     }
 
-    private static void WritePreWrite() => Console.WriteLine(s_preWrite);
+    private static void WritePreWrite() => Console.WriteLine(s_preWrite + "\n" + s_messagesPreWrite);
+    private static string GetFullPreWrite() => s_preWrite + "\n" + s_messagesPreWrite;
 
     private static void AddToPreWrite(string _preWriteAddition) => s_preWrite += _preWriteAddition;
+
+    private static void AddToMessagesPreWrite(string _preWriteAddition) => s_messagesPreWrite += _preWriteAddition;
 }
